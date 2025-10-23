4bb58af56807625ba9bf89fc8a357f005ea32e75
dimstyle <smilehacking27@gmail.com>
2025-10-23 23:16:15 +0700

    still develop delete button

--- src/Catatan.tsx
+++ src/Catatan.tsx
@@ -3,17 +3,22 @@ import TextareaAutosize from "react-textarea-autosize";
 import { ThemeContext } from "./App";
 
 type cardobjtype = Record<string, React.ReactNode>;
+type cardtexttype = Record<string, string>;
 let cardarr: cardobjtype = {};
+let title: cardtexttype = {}; 
 let cardarrcounter: number = 0;
 
 function Card({
   index,
+  
   setNote,
 }: {
   index: string;
   setNote: React.Dispatch<React.SetStateAction<cardobjtype>>;
 }): React.ReactElement | undefined {
   const Cardref = useRef<HTMLTextAreaElement | null>(null);
+  const CardText = useRef<HTMLParagraphElement | null>(null);
+  const [Text, setText]  = useState<string>();
   const themecontext = useContext(ThemeContext);
 
   if (!themecontext) return;
@@ -35,23 +40,35 @@ function Card({
           minRows={4}
           maxRows={4}
           onBlur={(e) => {
-            e.currentTarget.disabled = true;
+            if(!CardText.current) return
+            CardText.current.style.display = "block"
+            e.currentTarget.style.display = "none";
+            setText(e.currentTarget.value);
           }}
           onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              e.currentTarget.disabled = true;
+            if(!CardText.current) return
+            if (e.key === "Enter" ) {
+              CardText.current.style.display = "block"
+              e.currentTarget.style.display = "none"
+              setText(e.currentTarget.value);
             }
           }}
           ref={Cardref}
         />
+        <p ref={CardText} className="h-24 break-words whitespace-normal overflow-auto" style={{display : "none", scrollbarWidth : "none"}}>{Text}</p>
       </div>
       <div className="flex justify-end items-center gap-4 cursor-pointer mt-3">
         <button className="cursor-pointer">
           <img
             src={`edit-${theme}.svg`}
             onClick={() => {
-              if (!Cardref.current) return;
-              Cardref.current.disabled = false;
+              if (!Cardref.current || !CardText.current) return;
+              Cardref.current.value = CardText.current.innerText  
+              
+              CardText.current.innerText = "";
+              CardText.current.style.display = "none";
+              
+              Cardref.current.style.display = "block"
               Cardref.current.focus();
             }}
           />
@@ -59,11 +76,8 @@ function Card({
         <button
           className="cursor-pointer"
           onClick={() => {
-            let cardRemove: cardobjtype = {};
-            Object.entries(cardarr)
-              .filter((key) => key[0] != index)
-              .forEach((key) => (cardRemove[key[0]] = key[1]));
-            setNote(cardRemove);
+            delete cardarr[index];
+            setNote(cardarr);
           }}
         >
           <img src={`delete.svg`} />
@@ -75,14 +89,15 @@ function Card({
 
 export default function Catatan(): React.ReactElement | undefined {
   const firstNoteState: cardobjtype = {};
-  const [note, setNote] = useState<cardobjtype>(firstNoteState);
+  const [note, setNote] = useState<cardobjtype>(firstNoteState);  
 
   const themecontext = useContext(ThemeContext);
   if (!themecontext) return;
   const accent1 = themecontext.data.accent1;
 
   useEffect(() => {
-    cardarr = note;
+    cardarr = {...note};
+    console.log("bello")
   }, [note]);
   return (
     <div className="flex gap-10 w-full h-full overflow-hidden p-5 flex-wrap">
@@ -90,7 +105,7 @@ export default function Catatan(): React.ReactElement | undefined {
       .map((noteRecord) => noteRecord[1])}
       <button
         onClick={() => {
-          const index = `Card${cardarrcounter}`;
+          const index = `Card${cardarrcounter}`;  
           cardarrcounter++;
           setNote({
             ...note,
